fb60301865157240764a6460c00cbe3f205ebf53
Kazuki Ota <kaota@microsoft.com>
2023-10-23 11:01:31 +0900

    refactor

---
+++ EJokeBot/Options/AOAISettings.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EJokeBot.Options;
+
+public class AOAISettings
+{
+    [Required]
+    public string DeploymentName { get; set; } = "";
+
+    [Required]
+    public string Endpoint { get; set; } = "";
+
+    [Required]
+    public string ApiKey { get; set; } = "";
+}
---
+++ EJokeBot/Services/IJokeGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace EJokeBot.Services;
+
+public interface IJokeGenerator
+{
+    ValueTask<string> GenerateJokeAsync(string topic, CancellationToken cancellationToken);
+}
